5a0e8e5782d9e00c39b5173beb000331379225a0
frank.gairal <frank@autofi.io>
2022-03-30 18:44:21 +0200

    fix: fix deprecation warning

--- src/app/components/Header/SubNav.tsx
+++ src/app/components/Header/SubNav.tsx
@@ -1,4 +1,4 @@
-import { CSSProperties } from "react";
+import { CSSProperties, useRef } from "react";
 import { Transition, TransitionStatus } from "react-transition-group";
 
 import { NavSubItem } from "./types";
@@ -18,25 +18,30 @@ export const SubNav = ({
 }: {
   expanded: boolean;
   items: NavSubItem[];
-}) => (
-  <Transition in={expanded} timeout={TRANSITION_DURATION}>
-    {(state) => (
-      <ul
-        className={`fixed ml-4 -mt-6 py-2 z-header shadow bg-white rounded-lg text-neutral1 transition-opacity duration-${TRANSITION_DURATION}`}
-        style={{ ...TRANSITION_STYLES[state] }}
-      >
-        {items.map(({ content, label, href }) => (
-          <li key={label}>
-            <a
-              className="block px-4 py-2 hover:bg-neutral4"
-              href={href}
-              title={label}
-            >
-              {content ?? label}
-            </a>
-          </li>
-        ))}
-      </ul>
-    )}
-  </Transition>
-);
+}) => {
+  const ref = useRef(null);
+
+  return (
+    <Transition in={expanded} nodeRef={ref} timeout={TRANSITION_DURATION}>
+      {(state) => (
+        <ul
+          ref={ref}
+          className={`fixed ml-4 -mt-6 py-2 z-header shadow bg-white rounded-lg text-neutral1 transition-opacity duration-${TRANSITION_DURATION}`}
+          style={{ ...TRANSITION_STYLES[state] }}
+        >
+          {items.map(({ content, label, href }) => (
+            <li key={label}>
+              <a
+                className="block px-4 py-2 hover:bg-neutral4"
+                href={href}
+                title={label}
+              >
+                {content ?? label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
+    </Transition>
+  );
+};
