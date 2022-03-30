74b50fd2354c461a4602cb028d88cf081a71b791
frank.gairal <frank@autofi.io>
2022-03-30 18:42:01 +0200

    fix: undo breaking change

--- vite.config.ts
+++ vite.config.ts
@@ -5,21 +5,5 @@ import react from "@vitejs/plugin-react";
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   base: "",
-  css: {
-    postcss: {
-      plugins: [
-        {
-          postcssPlugin: "internal:charset-removal",
-          AtRule: {
-            charset: (atRule) => {
-              if (atRule.name === "charset") {
-                atRule.remove();
-              }
-            },
-          },
-        },
-      ],
-    },
-  },
   plugins: [react()],
 });
